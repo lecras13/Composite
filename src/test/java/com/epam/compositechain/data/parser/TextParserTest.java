cc7e4fa26b47221f50262d771c040c28baee9dfe
lecras13 <lecras@tut.by>
2020-11-13 23:18:57 +0300

    Update TextParserTest.java

--- src/test/java/com/epam/compositechain/data/parser/TextParserTest.java
+++ src/test/java/com/epam/compositechain/data/parser/TextParserTest.java
@@ -14,11 +14,13 @@ public class TextParserTest {
     private static final Component PARAGRAPH_SECOND = TestDataHelper.getSecondParagraph();
     private static final Component PARAGRAPH_THIRD = TestDataHelper.getThirdParagraph();
     private static final Component TEXT = TestDataHelper.getText();
+    private static final Component PARAGRAPH_ANSWER = TestDataHelper.getParagraphForThenAnswerText();
     private static final String SENTENCE_TO_PARSE = "It [ 1 6 + 6 * ] has. \n" +
             "PageMaker Ipsum. \n" +
             "It [ 6 2 - 5 * ] has. Bye. \n";
 
-    @Test
+
+   /* @Test
     public void testParseTextToParagraphsCompositeIsCorrect() {
         AbstractParser paragraph = Mockito.mock(ParagraphParser.class);
         when(paragraph.parse(anyString())).thenReturn(PARAGRAPH_FIRST).thenReturn(PARAGRAPH_SECOND).thenReturn(PARAGRAPH_THIRD);
@@ -27,5 +29,16 @@ public class TextParserTest {
         Component actual = textParser.parse(SENTENCE_TO_PARSE);
 
         Assert.assertEquals(TEXT, actual);
+    }*/
+
+    @Test
+    public void testParseTextToParagraphsCompositeIsCorrect() {
+        AbstractParser paragraph = Mockito.mock(ParagraphParser.class);
+        Parser textParser = new TextParser(paragraph);
+        when(paragraph.parse(anyString())).thenAnswer(invocation -> Leaf.leafWord(invocation.getArgument(0)));
+
+        Component actualComponent = textParser.parse(SENTENCE_TO_PARSE);
+
+        Assert.assertEquals(PARAGRAPH_ANSWER, actualComponent);
     }
 }
